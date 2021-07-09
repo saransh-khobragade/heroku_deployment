4830467e176a57dd4b7273ae1130664486f4d2ba
Saransh khobragade <saransh@Saranshs-MacBook-Air.local>
2021-07-09 23:16:51 +0530

    trial

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hi')
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+//basic get api with query string and query params
+app.get('/', async (req, res) => {
+    return res.status(400).json({err})
+});
+
+server.listen(process.env.PORT || 3000,()=>{
+    console.log('server running...at 3000');
+});
